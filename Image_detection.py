f9ecf46e3217d086362be3115fc89db562efee26
enriver <riverkeh@naver.com>
2021-06-20 15:39:49 +0900

    Breaking reCAPTCHA based on yoloV3

---
+++ Image_detection.py
@@ -0,0 +1,89 @@
+import PIL
+import cv2
+import numpy as np
+import core.utils as utils
+import tensorflow as tf
+from core.yolov3 import YOLOv3, decode
+from PIL import Image
+
+word_dict={'자전거':'bicycle',
+            '배':'boat',
+            '다리':'bridge',
+            '버스':'bus',
+            '자동차':'car',
+            '굴뚝':'chimney',
+            '횡단보도':'crosswalk',
+            '소화전':'fire hydrant',
+            '오토바이':'motorcycle',
+            '산':'mountain',
+            '야자수':'palm',
+            '계단':'stairs',
+            '택시':'taxi',
+            '트럭':'truck',
+            '신호등':'traffic Light',
+            '표지판':'traffic Sign',
+            }
+
+# Obect Dection 및 Image Division 에 사용할 클래스
+class Image_detection():
+    def __init__(self,keyword):
+        self.keyword=word_dict[keyword]
+        self.date=None
+
+    def set_date(self,date):
+        self.date=date
+
+    def yoloV3(self,address):
+        input_size = 416
+        image_path = address
+
+        input_layer = tf.keras.layers.Input([input_size, input_size, 3])
+        feature_maps = YOLOv3(input_layer)
+
+        original_image = cv2.imread(image_path)
+        original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+        original_image_size = original_image.shape[:2]
+
+        image_data = utils.image_preporcess(np.copy(original_image), [input_size, input_size])
+        image_data = image_data[np.newaxis, ...].astype(np.float32)
+
+        bbox_tensors = []
+        for i, fm in enumerate(feature_maps):
+            bbox_tensor = decode(fm, i)
+            bbox_tensors.append(bbox_tensor)
+
+        model = tf.keras.Model(input_layer, bbox_tensors)
+        utils.load_weights(model, "./yolov3.weights")
+        model.summary()
+
+        pred_bbox = model.predict(image_data)
+        pred_bbox = [tf.reshape(x, (-1, tf.shape(x)[-1])) for x in pred_bbox]
+        pred_bbox = tf.concat(pred_bbox, axis=0)
+        bboxes = utils.postprocess_boxes(pred_bbox, original_image_size, input_size, 0.3)
+        bboxes = utils.nms(bboxes, 0.45, method='nms')
+
+        # 이미지 그리드 분할
+        img=Image.open(image_path)
+        (img_h, img_w)=img.size
+        grid_h,grid_w=100,100
+
+        range_h,range_w=(int)(img_h/grid_h),(int)(img_w/grid_w)
+
+        image_grid=list()
+        i=0
+        for w in range(range_w):
+            for h in range(range_h):
+                image_grid.append(((h*grid_h, w*grid_w), ((h+1)*(grid_h), (w+1)*(grid_w))))
+
+        image,keyword_sector= utils.draw_bbox(original_image, bboxes, self.keyword)
+        image = Image.fromarray(image)
+        #image.show()
+
+        # 키워드 그리드 찾기
+        keyword_idx=utils.find_idx(image_grid,keyword_sector,range_h)
+
+        address_after="./captcha_image/AFTER_YOLO_{}.jpg".format(self.date)
+        image=np.array(image)
+        cv2.imwrite(address_after, image)
+
+        return keyword_idx
---
+++ captcha_breaking.py
@@ -0,0 +1,115 @@
+from selenium import webdriver
+#from selenium.webdriver.common.by import By
+#from selenium.webdriver.support.ui import WebDriverWait
+#from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+import time
+from Image_detection import *
+import random
+import urllib.request
+
+# 캡챠 이미지 영역으로 이동
+def move_to_imageFrame(web_driver):
+    imageFrame=web_driver.find_elements_by_tag_name("iframe")[2]
+    web_driver.switch_to.frame(imageFrame)
+    print("Image-based CAPTCHA 프레임 이동\n")
+    time.sleep(2)
+
+if __name__=="__main__":
+    driver=webdriver.Chrome('./chromedriver')
+    driver.implicitly_wait(1) # 대기
+    driver.get('https://patrickhlauke.github.io/recaptcha/')
+    driver.implicitly_wait(1)
+
+    # 캡챠 섹터 확인/프레임 영역
+    captchaSector=driver.find_element_by_class_name("g-recaptcha")
+    captchaFrame=captchaSector.find_elements_by_tag_name("iframe")[0]
+    
+    # 체크박스 클릭
+    time.sleep(2)
+    captchaFrame.click()
+    time.sleep(1)
+
+    i=1
+    while(True):
+        print("****"+str(i)+"번째 CAPTCHA****\n")
+        driver.switch_to.default_content()
+        # Image-based 캡챠 영역 이동
+        move_to_imageFrame(driver)
+
+        # 추출할 키워드
+        keyword=driver.find_element_by_xpath("/html/body/div/div/div[2]/div[1]/div[1]/div/strong").text
+        print("키워드 : "+keyword)  
+
+        # 이미지 영역
+        target_image=driver.find_element_by_xpath("/html/body/div/div/div[2]/div[2]/div/table/tbody/tr[1]/td[2]/div/div[1]/img")
+
+        imageSize=target_image.get_attribute("class")
+        print("이미지크기(n*n) : "+imageSize[-2:]+"\n")
+
+        imageSrc=target_image.get_attribute("src")
+        print("이미지소스 : "+imageSrc+"\n")
+        #print(type(imageSrc))
+        time.sleep(1)
+
+        
+        try:
+            img=urllib.request.urlopen(imageSrc).read()
+            date=time.strftime('%y-%m-%d %H-%M-%S')
+
+            address_origin="./captcha_image/ORIGIN_CAPTCHA_{}.jpg".format(date)
+
+            with open(address_origin,"wb") as f:
+                f.write(img)
+                print("캡챠 이미지를 저장하였습니다\n")
+
+        except urllib.error.HTTPError:
+            print("잘못된 url 주소입니다\n")
+
+        time.sleep(2)
+        # 추출한 이미지에서 Object Detection 진행
+        imgDetect=Image_detection(keyword)
+        imgDetect.set_date(date)
+
+        # imageSize 기준으로 분할하여 Object가 포함된 영역 추출
+        keyword_idx=imgDetect.yoloV3(address_origin)
+        print("클릭해야할 GRID 인덱스 : ",keyword_idx)
+
+        # 추출된 영역 Click event 로 Image-based CAPTCHA에 전송
+        time.sleep(1)
+        
+        print("이미지 클릭 실행\n")
+        table=driver.find_element_by_xpath("/html/body/div/div/div[2]/div[2]/div/table")
+        tbody=table.find_element_by_tag_name("tbody")
+        rows=tbody.find_elements_by_tag_name("tr")
+        time.sleep(2)
+        
+        for row_idx,row in enumerate(rows):
+            columns=row.find_elements_by_tag_name("td")
+
+            for col_idx,value in enumerate(columns):
+
+                if (row_idx,col_idx) in keyword_idx:
+                    print(row_idx,'-',col_idx," 를 클릭하였습니다 \n")
+                    value.click()
+                    time.sleep(1.5)
+                driver.implicitly_wait(1)
+
+        time.sleep(1.5)
+        btn=driver.find_element_by_xpath("/html/body/div/div/div[3]/div[2]/div[1]/div[2]/button")
+        
+        if btn.text=="확인":
+            btn.click()
+            break
+        else:
+            btn.click()
+
+        alertText=driver.find_element_by_xpath("/html/body/div/div/div[2]/div[5]")
+        print("Alert Text : "+alertText.text+"\n")
+        i+=1
+      
+
+    print("끝")
+    
+
+
